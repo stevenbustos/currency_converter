e66e03ecae5fe08172216d00ae24ae87771bd6b7
Steven Bustos <jsbustosb@unal.edu.co>
2019-03-18 17:13:27 -0500

    Connection to Database

---
+++ env.sh
@@ -0,0 +1,8 @@
+export API_PORT='10000';
+export FIXER_KEY='124eefc61f51325ae4b6c6665668f038';
+export DB_SERVER='localhost';
+export DB_PORT='27017';
+export DB_NAME='currency_converter_db';
+export DB_USER='valiu_admin';
+export DB_PASSWORD='c00k13m0nst3r';
+export DB_AUTH='admin';
