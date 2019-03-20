60250dd9bc1a644553314fe245cfc679ffaa58da
Steven Bustos <jsbustosb@unal.edu.co>
2019-03-19 19:10:57 -0500

    Second functional request done

--- api/controllers/rate_controller.js
+++ api/controllers/rate_controller.js
@@ -1,7 +1,57 @@
 const RateModel = require('../models/rates');
 
-// CRUD
+// Convert
+// Params:
+//  * String from_currency: ISO 4217 currency code
+//  * String to_currency: ISO 4217 currency code
+//  * Float money_amount: Amount of money A
+convert = async function (req, res) {
+    if (!req.query.from_currency || !req.query.to_currency || !req.query.money_amount){
+        return res.status(400).send('Please send all the values');
+    }
 
+    // Some definitions
+    var from = req.query.from_currency;
+    var to = req.query.to_currency;
+    var amount = req.query.money_amount;
+    var in_eur = 0;
+    var result = 0;
+
+    // Get all the currency rates from the latest save in the DB
+    var latest = await RateModel.findOne({}, {}, { sort: { '_id': -1 } }).exec();
+
+    // Check if the from and to currency exist in the database
+    if (!(from in latest.rates)) {
+        // from check
+        res.status(400).send(`The currency ${from} doesn't exist in the database`);
+    } else if (!(to in latest.rates)) {
+        // to check
+        res.status(400).send(`The currency ${to} doesn't exist in the database`);
+    } else {
+        // Since the rates stored in the DB are in base of EUR
+        // we need first to convert from the currency to EUR 
+
+        // If from_currency is EUR don't convert to EUR
+        if (from === latest.base) {
+            in_eur = amount;
+        } else {
+            in_eur = (amount / latest.rates[from]);
+            // res.send(`The amount in EUR is: ${in_eur}`);
+        }
+
+        // When we are already converted from the currency to EUR
+        // we need to convert from EUR to to_currency
+        if (to === 'EUR') {
+            result = in_eur;
+            res.status(200).send(`The amount ${amount} from ${from} to ${to} is: ${Math.ceil(result)}`);
+        } else {
+            result = (in_eur * latest.rates[to]);
+            res.status(200).send(`The amount ${amount} from ${from} to ${to} is: ${Math.ceil(result)}`);
+        }
+    }
+}
+
+// CRUD
 // Create
 rate_create = function (req, res) {
     if (!req.body) {
@@ -75,5 +125,6 @@ module.exports = {
     rate_read,
     rate_read_id,
     rate_update,
-    rate_delete
+    rate_delete,
+    convert
 }
